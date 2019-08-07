c826b86f2f01deacc02685bb05b21e949a53ea55
LZx-space <L.mikespace@outlook.com>
2019-08-07 22:36:23 +0800

    modif common-core package

---
+++ blog/src/main/java/space/nature/JavaWebApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019, LZx
+ */
+
+package space.nature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebApplication.class, args);
+	}
+
+}
---
+++ blog/src/test/java/space/nature/JavaWebApplicationTests.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019, LZx
+ */
+
+package space.nature;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
