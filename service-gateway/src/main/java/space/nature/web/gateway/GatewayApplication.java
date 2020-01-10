7e95832afc0309494c3e9f0fe1e770daa8a7f8c0
LZx-space <L.mikespace@outlook.com>
2020-01-10 23:11:20 +0800

    由zuul网关切换为spring-cloud-gateway

--- service-gateway/src/main/java/space/nature/web/gateway/GatewayApplication.java
+++ service-gateway/src/main/java/space/nature/web/gateway/GatewayApplication.java
@@ -5,17 +5,13 @@
 package space.nature.web.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.reactive.error.ErrorWebFluxAutoConfiguration;
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import space.nature.web.gateway.infrastructure.filters.JsonExceptionHandler;
 
-@SpringBootApplication(exclude = ErrorWebFluxAutoConfiguration.class)
+@SpringBootApplication(exclude = {ErrorWebFluxAutoConfiguration.class})
 public class GatewayApplication {
 
     public static void main(String[] args) {
