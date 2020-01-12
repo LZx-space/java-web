c44146657a2b18c5cf748dca0e870c803231751f
LZx <l.mikespace@outlook.com>
2020-01-12 13:58:54 +0800

    blog rename class

--- service-blog/src/main/java/space/nature/web/blog/BlogApplication.java
+++ service-blog/src/main/java/space/nature/web/blog/BlogApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaWebApplication {
+public class BlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaWebApplication.class, args);
+        SpringApplication.run(BlogApplication.class, args);
     }
 
 }
--- service-blog/src/test/java/space/nature/web/BlogApplicationTests.java
+++ service-blog/src/test/java/space/nature/web/BlogApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JavaWebApplicationTests {
+public class BlogApplicationTests {
 
     @Test
     public void contextLoads() {
